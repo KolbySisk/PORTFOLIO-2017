bfa1c5f564f576300fa2f334dabd2b2508c08bcf
Kolby Sisk <Kolby Sisk>
2017-01-21 00:55:09 -0700

    single quote - &#39;

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -11,7 +11,7 @@ class Home extends Component{
 							<div className="top-left-i-will-refactor-this">
 								<h2>I am</h2>
 								<p>a tech junky who loves to learn new technologies and software. I started by learning Photoshop around the age of 13 which transitioned me into designing websites, and eventually got me into web development.</p>
-								<p>Professionally I’m all about Angular, React, Cordova, C#, and PHP.</p>
+								<p>Professionally I&#39;m all about Angular, React, Cordova, C#, and PHP.</p>
 								<p>I have years of experience with both the LAMP and Microsoft Stacks. I'm comfortable doing dev-ops, front-end, and back-end in both environments.</p>
 							</div>
 							<div className="top-right-for-now">
