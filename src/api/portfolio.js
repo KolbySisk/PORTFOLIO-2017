24e87ff7871416cb4eb07053947bd36996b9bfca
Kolby Sisk <Kolby Sisk>
2017-01-30 21:55:34 -0700

    setting production api url

--- src/api/portfolio.js
+++ src/api/portfolio.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_ENDPOINT = 'http://localhost:57144/api/stuff'
+const API_ENDPOINT = 'http://www.kolbysisk.com/api/stuff'
 
 export const getStuff = () => {
   return axios.get(API_ENDPOINT)
