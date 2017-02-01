db0acc84837e6d9ac46189889a5db2f61ada41ca
Kolby Sisk <Kolby Sisk>
2017-01-31 23:12:22 -0700

    button style updates

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -16,7 +16,7 @@ const Home = ({technologies}) => (
 					<p>a tech junky who loves to learn new technologies and software. I started by learning Photoshop around the age of 13 which transitioned me into designing websites, and eventually got me into web development.</p>
 					<p>Professionally I&#39;m all about Angular, React, Cordova, C#, and PHP.</p>
 					<p>I have years of experience with both the LAMP and Microsoft Stacks. I&#39;m comfortable doing dev-ops, front-end, and back-end in both environments.</p>
-					<p><a href="https://www.resume.com/" target="_blank">resume</a></p>
+					<button>resume</button>
 				</Intro>
 				<TechnologyList technologies={technologies} />
 			</div>
