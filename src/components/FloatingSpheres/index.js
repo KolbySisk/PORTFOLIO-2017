9ef38086e8117a665c382d8364dc52d2acbb322d
Kolby Sisk <Kolby Sisk>
2017-02-27 00:03:40 -0700

    refactoring and tyding floating sphere component

---
+++ src/components/FloatingSpheres/index.js
@@ -0,0 +1,160 @@
+import React, { Component } from 'react'
+import THREELib from 'three-js'
+import './styles.scss'
+
+class FloatingSpheres extends Component {
+  componentDidMount() {
+    let banner = document.querySelector('.site-banner')
+    let container = this.refs.container
+    let camera, scene, renderer
+    let mesh
+    let spheres = []
+    let mouseX = 0, mouseY = 0
+    let direction = -1
+    let modes = {
+      first: 1,
+      second: 2,
+      third: 3,
+      fourth: 4
+    }
+    let mode = modes.first
+    let clickCount = 0
+
+    document.addEventListener('mousemove', onDocumentMouseMove, false)
+    window.addEventListener('resize', onWindowResize, false)
+    banner.addEventListener("click", toggleMode)
+
+    setTimeout(() => {
+      init()
+      animate()
+    })
+
+    function toggleMode(){
+      clickCount ++
+      mode === Object.keys(modes).length ? mode = 1 : mode ++
+    }
+
+    function init() {
+      let THREE = THREELib()
+
+      camera = new THREE.PerspectiveCamera(60, banner.offsetWidth / banner.offsetHeight, 1, 10000)
+      scene = new THREE.Scene()
+
+      let geometry = new THREE.SphereBufferGeometry(11, 32, 16)
+      let textureCube = new THREE.CubeTextureLoader()
+        .setPath('banner-texture/')
+        .load([ 'px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg' ])
+
+      textureCube.mapping = THREE.CubeRefractionMapping
+
+      let material = new THREE.MeshBasicMaterial({ envMap: textureCube, refractionRatio: 0.94, metalness: 1.0 })
+
+      for (let i = 0; i < 200; i ++) {
+        let sphere = new THREE.Mesh(geometry, material)
+        sphere.position.x = Math.random() * 10000 - 5000
+        sphere.position.y = Math.random() * 10000 - 5000
+        sphere.position.z = Math.random() * 10000 - 5000
+        sphere.scale.x = sphere.scale.y = sphere.scale.z = Math.random() * 3
+
+        scene.add(sphere)
+        spheres.push(sphere)
+      }
+
+      renderer = new THREE.WebGLRenderer({ alpha: true })
+      renderer.setPixelRatio(window.devicePixelRatio)
+      renderer.setSize(banner.offsetWidth, banner.offsetHeight)
+      container.appendChild(renderer.domElement)
+    }
+
+    function onWindowResize() {
+      camera.aspect = banner.offsetWidth / banner.offsetHeight
+      camera.updateProjectionMatrix()
+      renderer.setSize(banner.offsetWidth, banner.offsetHeight)
+    }
+
+    function onDocumentMouseMove(event) {
+      mouseX = event.clientX
+      mouseY = event.clientY
+    }
+
+    function animate() {
+      requestAnimationFrame(animate)
+      render()
+    }
+
+    function moveToPoint(sphere, x, y, speed){
+      if(x) sphere.position.x < x ? sphere.position.x += speed : sphere.position.x -= speed
+      if(y) sphere.position.y < y ? sphere.position.y += speed : sphere.position.y -= speed
+    }
+
+    let timer = 0.01
+    let speedOfTime = 0.01
+    let timerStop = false
+
+    function moveSpheres(){
+      if(clickCount > 7) speedOfTime = .1
+      if(clickCount > 14) speedOfTime = .001
+      if(!timerStop) timer += speedOfTime
+
+      let speed = 20
+
+      if(mode === modes.first){
+        timerStop = false
+        spheres.forEach((sphere, i) => {
+          sphere.position.x = banner.offsetWidth * Math.cos(timer * direction + i)
+          sphere.position.y = banner.offsetHeight * Math.sin(timer * direction + i)
+        })
+      }
+
+      if(mode === modes.second){
+        timerStop = true
+        spheres.forEach((sphere, i) => {
+          sphere['pausedPosition'] = { x: sphere.position.x, y: sphere.position.y }
+        })
+      }
+
+      if(mode === modes.third){
+        spheres.forEach((sphere, i) => {
+          let pointToMoveTo = banner.offsetHeight/2
+
+          function moveTowardsMiddle(){
+            moveToPoint(sphere, null, pointToMoveTo, speed)
+          }
+
+          if(sphere.position.y < pointToMoveTo-speed || sphere.position.y > pointToMoveTo+speed) moveTowardsMiddle()
+        })
+      }
+
+      if(mode === modes.fourth){
+        spheres.forEach((sphere, i) => {
+          if(sphere.position.y < sphere.pausedPosition.y-speed || sphere.position.y > sphere.pausedPosition.y+speed) 
+            moveToPoint(sphere, null, sphere.pausedPosition.y, speed)
+
+          if(sphere.position.x < sphere.pausedPosition.x-speed || sphere.position.x > sphere.pausedPosition.x+speed) 
+            moveToPoint(sphere, sphere.pausedPosition.x, null, speed)
+        })
+      }
+    }
+
+    function render() {
+      let x = (mouseX + 1000 - camera.position.x) * .5
+      let y = (mouseY - 500 - camera.position.y) * .9
+
+      moveSpheres()
+
+      camera.position.x += x
+      camera.position.y += y
+      camera.lookAt(scene.position)
+
+      renderer.render(scene, camera)
+    }
+  }
+
+  render(){
+    return(
+      <div ref="container"></div>
+    )
+  }
+}
+
+export default FloatingSpheres
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,152 +1,22 @@
-﻿import React, { Component } from 'react'
+﻿import React from 'react'
 import Parallaxer from '../../containers/Parallaxer'
-import THREELib from 'three-js'
+import FloatingSpheres from '../FloatingSpheres'
 import './styles.scss'
 
-class Header extends Component {
-  componentDidMount() {
-    let container
-    let camera, scene, renderer
-    let mesh, lightMesh, geometry
-    let spheres = []
-    let directionalLight, pointLight
-    let mouseX = 0, mouseY = 0
-    let direction = -1
-    let banner = this.refs.banner
-    let modes = {
-      normal: 1,
-      second: 2
-    }
-    let mode = modes.normal
-
-    document.addEventListener('mousemove', onDocumentMouseMove, false)
-    window.addEventListener('resize', onWindowResize, false)
-    banner.addEventListener("click", toggleMode);
-
-    setTimeout(() => {
-      init()
-      animate()
-    })
-
-    function toggleMode(){
-      mode === Object.keys(modes).length ? mode = 1 : mode ++
-    }
-
-    function init() {
-      let THREE = THREELib()
-      
-      container = document.createElement('div')
-      banner.insertBefore(container, banner.firstChild)
-
-      camera = new THREE.PerspectiveCamera(60, banner.offsetWidth / banner.offsetHeight, 1, 10000)
-      scene = new THREE.Scene()
-
-      let geometry = new THREE.SphereBufferGeometry(11, 32, 16)
-      let textureCube = new THREE.CubeTextureLoader()
-        .setPath('banner-texture/')
-        .load([ 'px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg' ])
-
-      textureCube.mapping = THREE.CubeRefractionMapping
-
-      let material = new THREE.MeshBasicMaterial({ envMap: textureCube, refractionRatio: 0.94, metalness: 1.0 })
-
-      for (let i = 0; i < 200; i ++) {
-        let mesh = new THREE.Mesh(geometry, material)
-
-        mesh.position.x = Math.random() * 10000 - 5000
-        mesh.position.y = Math.random() * 10000 - 5000
-        mesh.position.z = Math.random() * 10000 - 5000
-        mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3
-
-        scene.add(mesh)
-        spheres.push(mesh)
-      }
-
-      renderer = new THREE.WebGLRenderer({ alpha: true })
-      renderer.setPixelRatio(window.devicePixelRatio)
-      renderer.setSize(banner.offsetWidth, banner.offsetHeight)
-      container.appendChild(renderer.domElement)
-    }
-
-    function onWindowResize() {
-      camera.aspect = banner.offsetWidth / banner.offsetHeight
-      camera.updateProjectionMatrix()
-      renderer.setSize(banner.offsetWidth, banner.offsetHeight)
-    }
-
-    function onDocumentMouseMove(event) {
-      mouseX = event.clientX
-      mouseY = event.clientY
-    }
-
-    function animate() {
-      requestAnimationFrame(animate)
-      render()
-    }
-
-    function moveSpheres(){
-      let timer = 0.0001 * Date.now()
-      let speed = 5
-
-      if(mode === modes.normal){
-        spheres.forEach((sphere, i) => {
-          sphere.position.x = banner.offsetWidth * Math.cos(timer * speed * direction + i)
-          sphere.position.y = banner.offsetHeight * Math.sin(timer * speed * direction + i)
-        })
-      }
-
-      if(mode === modes.second){
-        speed = 10
-        spheres.forEach((sphere, i) => {
-          function moveTowardsMiddle(){
-            sphere.position.y < 300 ? sphere.position.y += speed : sphere.position.y -= speed
-          }
-
-          if(sphere.position.y < 290 || sphere.position.y > 310) moveTowardsMiddle()
-        })
-      }
-
-
-      if(mode === modes.third){
-        spheres.forEach((sphere, i) => {
-          function moveTowardsMouse(){
-          }
-
-          moveTowardsMouse()
-        })
-      }
-    }
-
-    function render() {
-      let x = (mouseX / banner.offsetWidth) * 100
-      let y = (mouseY / banner.offsetHeight) * 100
-
-      moveSpheres()
-
-      camera.position.x += (mouseX + 1000 - camera.position.x) * .9 + 30
-      camera.position.y += (mouseY - 50 - camera.position.y) * .9 + 30
-
-      camera.lookAt(scene.position)
-      renderer.render(scene, camera)
-    }
-  }
-
-  render(){
-    return(
-      <header className="site-header">
-        <div className="site-banner" ref="banner">
-          <img src={'/images/logo.png'} alt="my logo - kolby sisk :)#" className="site-banner__logo" draggable="false" ref="logo"/>
-          <Parallaxer>
-            <picture>
-              <source srcSet="/images/home-banner.jpg" media="(min-width: 1280px)" />
-              <source srcSet="/images/home-banner__medium.jpg" media="(min-width: 768px)" />
-              <img src="/images/home-banner__small.jpg" className="site-banner__image" alt="sweet banner" />  
-            </picture>
-          </Parallaxer>
-        </div>
-      </header>
-    )
-  }
-}
+const Header = () => (
+  <header className="site-header">
+    <div className="site-banner">
+      <FloatingSpheres />
+      <img src={'/images/logo.png'} alt="my logo - kolby sisk :)#" className="site-banner__logo" draggable="false"/>
+      <Parallaxer>
+        <picture>
+          <source srcSet="/images/home-banner.jpg" media="(min-width: 1280px)" />
+          <source srcSet="/images/home-banner__medium.jpg" media="(min-width: 768px)" />
+          <img src="/images/home-banner__small.jpg" className="site-banner__image" alt="sweet banner" />  
+        </picture>
+      </Parallaxer>
+    </div>
+  </header>
+)
 
 export default Header
