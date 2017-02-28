1b63169a227dc70143ebeb86ddd7e941f255647e
Kolby Sisk <Kolby Sisk>
2017-02-27 21:55:50 -0700

    stuff card effects

--- src/components/StuffCard/index.js
+++ src/components/StuffCard/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import './styles.scss'
 
 const StuffCard = ({stuff, active, onClick}) => (
-  <article className="stuff-card" onClick={onClick}>
+  <article className="stuff-card" onClick={ onClick }>
     <div className="stuff-card__body">
-      <img src={stuff.imagePath} alt={stuff.name} draggable="false"/>
+      <img src={ stuff.imagePath } alt={ stuff.name } style={{ backgroundImage: stuff.imagePath }} draggable="false"/>
     </div>
     <footer className="stuff-card__footer">
-      <h2>{stuff.name}</h2>
+      <h2>{ stuff.name }</h2>
     </footer>
   </article>
 )
