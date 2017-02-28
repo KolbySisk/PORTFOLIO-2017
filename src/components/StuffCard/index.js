764134d0884548f1c1269b60cdf04b0c1acdfb56
Kolby Sisk <Kolby Sisk>
2017-02-27 23:29:08 -0700

    stuff card link styles

--- src/components/StuffCard/index.js
+++ src/components/StuffCard/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import FontAwesome from 'react-fontawesome'
 import './styles.scss'
 
-const StuffCard = ({stuff, active, onClick}) => (
+const StuffCard = ({ stuff, active, onClick }) => (
   <article className="stuff-card" onClick={ onClick }>
     <div className="stuff-card__body">
       <img src={ stuff.imagePath } alt={ stuff.name } style={{ backgroundImage: stuff.imagePath }} draggable="false"/>
     </div>
     <footer className="stuff-card__footer">
       <h2>{ stuff.name }</h2>
+      { stuff.link ? <a className="link" href={ stuff.link }><FontAwesome name='external-link' /></a> : '' }
     </footer>
   </article>
 )
