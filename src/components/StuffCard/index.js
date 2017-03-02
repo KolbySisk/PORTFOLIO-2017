e5f47a92f78f252d13aadc60050f8afed0e1f391
Kolby Sisk <kolby@datausa.com>
2017-03-02 11:49:34 -0700

    redesigning stuff card

--- src/components/StuffCard/index.js
+++ src/components/StuffCard/index.js
@@ -5,10 +5,10 @@ import './styles.scss'
 const StuffCard = ({ stuff, active, onClick }) => (
   <article className="stuff-card" onClick={ onClick }>
     <div className="stuff-card__body">
+      <h2 className="name">{ stuff.name }</h2>
       <img src={ stuff.imagePath } alt={ stuff.name } style={{ backgroundImage: stuff.imagePath }} draggable="false"/>
     </div>
     <footer className="stuff-card__footer">
-      <h2>{ stuff.name }</h2>
       <div className="actions">
         { stuff.link ? <a className="link" href={ stuff.link }><FontAwesome name='external-link' /></a> : '' }
         <a className="info"><FontAwesome name='info' /></a>
