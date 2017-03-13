025695e0ef6874bffe61ac618790d94ace65024a
Kolby Sisk <Kolby Sisk>
2017-03-12 21:38:12 -0600

    stuff hover styles and stuff role

--- src/components/StuffCard/index.js
+++ src/components/StuffCard/index.js
@@ -5,7 +5,10 @@ import './styles.scss'
 const StuffCard = ({ stuff, active, onClick }) => (
   <article className="stuff-card" onClick={ onClick }>
     <div className="stuff-card__body">
-      <h2 className="name">{ stuff.name }</h2>
+      <div className="centerer">
+        { stuff.role ? <h3 className="role">{ stuff.role }</h3> : '' }
+        <h2 className="name">{ stuff.name }</h2>
+      </div>
       <img src={ stuff.imagePath } alt={ stuff.name } style={{ backgroundImage: stuff.imagePath }} draggable="false"/>
     </div>
     <footer className="stuff-card__footer">
