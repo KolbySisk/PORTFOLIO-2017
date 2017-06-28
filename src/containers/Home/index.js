7e798c659454f977a62ebd2f2294367a7cf4df85
Kolby Sisk <kolbysisk@Kolbys-MacBook-Pro.local>
2017-06-28 00:49:19 -0600

    removed resume button

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -21,7 +21,7 @@ class Home extends Component {
 							<p>a tech junky who loves to learn new technologies and software. I started by learning Photoshop around the age of 13 which transitioned me into designing websites, and eventually got me into web development.</p>
 							<p>Professionally I&#39;m all about Angular, React, Cordova, C#, and PHP.</p>
 							<p>I have years of experience with both the LAMP and Microsoft Stacks. I&#39;m comfortable doing dev-ops, front-end, and back-end in both environments.</p>
-							<button>resume</button>
+							{/*<button>resume</button>*/}
 						</Intro>
 						<TechnologyList technologies={technologies} />
 					</div>
